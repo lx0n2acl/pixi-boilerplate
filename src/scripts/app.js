55ccc4ee883f18a1cb6549e600eeb2f645d23cd5
David Pukalski <dev@davidpukalski.com>
2019-02-18 17:28:37 -0500

    cleanup. added test sprite. added resize logic from kittykatattack.

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,29 +1,39 @@
+var PIXI = require("pixi.js");
+var { scaleToWindow } = require("./utils.js");
 
-var PIXI = require('pixi.js');
-
-let type = "WebGL"
+let type = "WebGL";
 if (!PIXI.utils.isWebGLSupported()) {
-    type = "canvas"
+  type = "canvas";
 }
 
 //Create a Pixi Application
-let app = new PIXI.Application({ width: 800, height: 600 });
-app.renderer.backgroundColor = 0x061639;
+let app = new PIXI.Application({
+  width: 800,
+  height: 600,
+  antialias: true,
+  transparent: false,
+  resolution: 1
+});
+
+// set window
+app.renderer.backgroundColor = 0x000000;
+app.renderer.view.style.position = "absolute";
+app.renderer.view.style.display = "block";
 app.renderer.autoResize = true;
+app.renderer.resize(window.innerWidth, window.innerHeight);
+
+// listens for resize events
+window.addEventListener("resize", function(event) {
+  scaleToWindow(app.renderer.view);
+});
 
 //Add the canvas that Pixi automatically created for you to the HTML document
 document.body.appendChild(app.view);
 
-PIXI.loader
-  .add("assets/test.png")
-  .load(setup);
+PIXI.loader.add("assets/test_sprite.png").load(setup);
 
 function setup() {
-  let sprite = new PIXI.Sprite(
-    PIXI.loader.resources["assets/test.png"].texture
-  );
-  
+  let sprite = new PIXI.Sprite(PIXI.loader.resources["assets/test_sprite.png"].texture);
+
   app.stage.addChild(sprite);
 }
-
-
--- webpack.config.js
+++ webpack.config.js
@@ -1,52 +1,48 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin-advanced');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin-advanced");
 
 module.exports = {
-	mode: 'development',
-	context: path.resolve(__dirname, 'src'),
-	entry: {
-		app: './scripts/app.js',
-	},
-	devtool: 'inline-source-map',
-	devServer: {
-		contentBase: './dist'
-	},
-	module: {
-		rules: [
-			{
-				test: /\.css$/,
-				use: [
-					'style-loader',
-					'css-loader'
-				]
-			},
-			{
-				test: /\.(png|svg|jpg|gif)$/,
-				use: [
-					'file-loader'
-				]
-			}
-		]
-	},
-	plugins: [
-		new CleanWebpackPlugin(['dist']),
-		new HtmlWebpackPlugin({
-			template: './index.html'
-		}),
-		new CopyWebpackPlugin([
-			{
-				from: './assets/**/**',
-				flatten: true,
-			},
-			// {
-			// 	from: 'index.html'
-			// },
-		]),
-	],
-	output: {
-		filename: '[name].bundle.js',
-		path: path.resolve(__dirname, 'dist')
-	}
+  mode: "development",
+  context: path.resolve(__dirname, "src"),
+  entry: {
+    app: "./scripts/app.js"
+  },
+  devtool: "inline-source-map",
+  devServer: {
+    contentBase: "./dist"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ["file-loader"]
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(["dist"]),
+    new HtmlWebpackPlugin({
+      template: "./index.html"
+    }),
+    new CopyWebpackPlugin([
+      {
+        from: "./assets/**/**",
+        to: "assets/",
+        flatten: true
+      },
+      {
+        from: "index.html"
+      }
+    ])
+  ],
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist")
+  }
 };
